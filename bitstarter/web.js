29721b84276d61e1418d79377220131b3318c2de
Nick Nikzat <nnikzat@gmail.com>
2013-07-07 21:15:04 +0000

    index.html... 4th time

--- bitstarter/web.js
+++ bitstarter/web.js
@@ -1,10 +1,19 @@
 var express = require('express');
+fs = require('fs');
 
 var app = express.createServer(express.logger());
+myBuff = new Buffer(256);
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+fs.readFileSync('./index.html'), function (err, data) {
+   if (err) throw err;
+   len = myBuff.write(data);
+   console.log(myBuff.toString('utf8', 0, len));
 });
+
+
+//app.get('/', function(request, response) {
+//  response.send('Hello World 2!');
+//});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
