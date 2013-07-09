858d482e9ac04e6276c8f69e343bb3b4206f4975
Nick Nikzat <nnikzat@gmail.com>
2013-07-09 20:34:16 +0000

    trying to fix 503 responses

--- bitstarter/web.js
+++ bitstarter/web.js
@@ -18,11 +18,11 @@ fs.exists(fileName, function(exists) {
   }
 });
 
-//app.get('/', function(request, response) {
-//  response.send('Hello World 2!');
-//});
+app.get('/', function(request, response) {
+  response.send('Hello World 2!');
+});
 
-//var port = process.env.PORT || 5000;
-//app.listen(port, function() {
-//  console.log("Listening on " + port);
-//});
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
