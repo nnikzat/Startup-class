8aeb29ad8b2fd456b182da952ca9a66ef4f2b094
Nick Nikzat <nnikzat@gmail.com>
2013-07-09 19:55:06 +0000

    removed notes from web.js

--- bitstarter/web.js
+++ bitstarter/web.js
@@ -18,20 +18,11 @@ fs.exists(fileName, function(exists) {
   }
 });
 
-/* myBuff = new Buffer(256);
-
-fs.readFileSync('index.html'), function (err, data) {
-   if (err) throw err;
-   len = myBuff.write(data);
-   console.log(myBuff.toString('utf8', 0, len));
-};
-*/
-
 //app.get('/', function(request, response) {
 //  response.send('Hello World 2!');
 //});
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+//var port = process.env.PORT || 5000;
+//app.listen(port, function() {
+//  console.log("Listening on " + port);
+//});
