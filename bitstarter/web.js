be98d52a4cb819cdf3df95586852b74e780f995d
Nick Nikzat <nnikzat@gmail.com>
2013-07-12 04:49:55 +0000

    bitstarter

--- bitstarter/web.js
+++ bitstarter/web.js
@@ -11,6 +11,9 @@ fs.exists(fileName, function(exists) {
         fs.read(fd, buffer, 0, buffer.length, null, function(error, bytesRead, buffer) {
           var data = buffer.toString("utf8", 0, buffer.length);
           console.log(data);
+          app.get('/', function(request, response) {
+             response.send(data);
+          });
           fs.close(fd);
         });
       });
@@ -18,9 +21,9 @@ fs.exists(fileName, function(exists) {
   }
 });
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
+//app.get('/', function(request, response) {
+//  response.send('Hello World 2!');
+//});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
