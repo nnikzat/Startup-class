87cd470a8668157c05d706702817d97e565bed0b
Nick Nikzat <nnikzat@gmail.com>
2013-07-14 19:10:57 +0000

    changed to listen on port 8080

--- bitstarter/web.js
+++ bitstarter/web.js
@@ -25,7 +25,7 @@ fs.exists(fileName, function(exists) {
 //  response.send('Hello World 2!');
 //});
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
